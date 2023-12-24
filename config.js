ea66fc3624ba58148dd15f05fc649fafb211de57
Whatsappbor <113112104+Whatsappbor@users.noreply.github.com>
2023-12-24 20:59:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['94703326218', 'Nawindu Sathsara', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
